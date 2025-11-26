7883744a0d2539013e8c2d4b373883fa11fb6fab
GuiSantosCode <guitiesco@gmail.com>
2025-11-26 16:45:11 -0300

    ex 08 e 09

---
+++ guanabara-python/001-010/009.py
@@ -0,0 +1,5 @@
+num_multiplicado = int(input('Qual número você quer a taboada? '))
+
+for i in range(1, 11):
+    resultado = num_multiplicado * i
+    print(f'{num_multiplicado} X {i} = {resultado}')
