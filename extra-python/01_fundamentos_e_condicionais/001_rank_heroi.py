7de2d207f589a64abef56ba066728e5f4843c7f9
GuiSantosCode <guitiesco@gmail.com>
2025-11-29 16:54:10 -0300

    extra-python criada

---
+++ extra-python/01_fundamentos_e_condicionais/001_rank_heroi.py
@@ -0,0 +1,26 @@
+print('RANK DE HERÓI!!!')
+
+xp = int(input('Quanto de xp seu herói tem? '))
+
+print('O seu rank de herói é: ')
+
+if xp < 0:
+    print('Erro de execução, valor inválido!')
+elif xp < 1000:
+    print('Ferro!')
+elif xp < 2000:
+    print('Bronze!')
+elif xp < 5000:
+    print('Prata!')
+elif xp < 8000:
+    print('Ouro!')
+elif xp < 9000:
+    print('Platina!')
+elif xp < 10000:
+    print('Diamante!')
+else:
+    print('Mestre!')
+
+
+
+
