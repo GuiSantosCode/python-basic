e3468fbc1f5ee41587ca9a7fb030e545e3e97b9f
GuiSantosCode <guitiesco@gmail.com>
2025-12-07 15:27:18 -0300

    a

--- curso_em_video/exercicios_021_030/028.py
+++ curso_em_video/exercicios_021_030/028.py
@@ -1,17 +1,1 @@
-nome_receita = (input('É receita do que? '))
-ingredientes = int(input('Quantos ingredientes vai usar? '))
 
-receita = f'''
-========================================================
-            Receita de {nome_receita}
-========================================================
-'''
-
-i = 0
-while i < ingredientes:
-    receita += '\n'
-    receita += (input('Digite um ingrediente: '))
-    i += 1 
-
-print(receita)
-print('')
