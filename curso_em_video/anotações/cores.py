73d15b849aa1226ac92db1faa896306c22187ccc
GuiSantosCode <guitiesco@gmail.com>
2025-12-10 17:56:21 -0300

    ohsim

--- curso_em_video/anotações/cores.py
+++ curso_em_video/anotações/cores.py
@@ -1,3 +1,16 @@
 '''
+PADRÃO ANSI:
+    escape sequence
 
+\033[0;33;44m
+    entre [ e m
+    0 = style
+    33 = text
+    44 = back
+
+exemplo: print('\033[1;31;43mOlá, Mundo!')
+style padrao, text vermelho, e back amarelo.
+    
+7 = style
+    inverte as cores do text e back
 '''
