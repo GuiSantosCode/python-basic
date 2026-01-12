092a2555db7d7c7771b6dead7aea00219d63c09a
GuiSantosCode <guitiesco@gmail.com>
2026-01-12 18:11:21 -0300

    ok

--- curso_em_video/exercicios_061_070/ex067.py
+++ curso_em_video/exercicios_061_070/ex067.py
@@ -5,11 +5,9 @@ while True:
     numero = int(input('Digite um número: '))
     if numero < 0:
         break
-    
-    print('-' * 20)
+
     while multiplicador <= 10:
         print(f'{numero:2} X {multiplicador:2} = {numero*multiplicador:3}')
-        multiplicador += 1    
-    print('-' * 20)
-    
+        multiplicador += 1   
+        
 print('Números negativos encerram o programa!')
