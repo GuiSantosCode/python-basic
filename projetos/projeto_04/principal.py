cddd52cc6bdd6675995779be5f64113e9f4771b4
GuiSantosCode <guitiesco@gmail.com>
2026-05-30 18:37:21 -0300

    commit

---
+++ projetos/projeto_04/principal.py
@@ -0,0 +1,8 @@
+# Organizar faculdade
+
+# Em qual ritmo estudar pra entregar as disciplinas no prazo
+    # Calcular tempo gasto de cada tema
+        # Sistema de checklist para cada arquivo dos tópicos de cada tema de todas as disciplinas
+        # Colocar sistema de justificativa de falta
+    # Calcular calcular tempo gasto de cada disciplina
+    
